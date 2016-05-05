114fcaa1f684e9ac580ba30201368b826b2a8fa4
Kim Kyeng Jun <juniair.kim@gmail.com>
2016-05-05 23:52:31 +0900

    update

--- tcp/tcp_client.c
+++ tcp/tcp_client.c
@@ -44,11 +44,6 @@ int main(int argc, char* argv[])
 		error_handling("connet() error");
 	}
 	
-	//printf("Welcome to 'Hello Server'\n");
-	//sprintf(clnt_msg, "Hi Server!\n");
-	//printf("%s: Hi Client!\n", SERVER_NAME);
-	//write(sock, clnt_msg, sizeof(clnt_msg));
-
 	while (1)
 	{
 		printf("%s: ", CLIENT_NAME);
@@ -61,7 +56,7 @@ int main(int argc, char* argv[])
 		}
 		
 		write(sock, clnt_msg, sizeof(clnt_msg));
-		read(sock, serv_msg, sizeof(serv_msg))
+		read(sock, serv_msg, sizeof(serv_msg));
 		if (!strcmp(serv_msg, "q\n") || !strcmp(serv_msg, "Q\n"))
 		{
 			printf("Server down\n");
